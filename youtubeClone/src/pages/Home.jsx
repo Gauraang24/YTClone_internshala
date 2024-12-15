8c683ab151b7a7a51987a353b6e6ca4a969885f6
Gauraang24 <garryappdroid@gmail.com>
2024-12-15 22:00:26 +0530

    Home Page : GetVideoList api integrated

--- youtubeClone/src/pages/Home.jsx
+++ youtubeClone/src/pages/Home.jsx
@@ -1,53 +1,56 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import VideoCard from "../components/CustomComponent/VideoCard";
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { unwrapResult } from "@reduxjs/toolkit";
+import { getAllVideosFunc } from "../store/slices/videoSlice";
 
-const sampleData = [
-  {
-    videoId: "video01",
-    title: "Learn React in 30 Minutes",
-    thumbnailUrl:
-      "https://images.unsplash.com/photo-1497015455546-1da71faf8d06?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    channelName: "Gaurang More",
-    channelId: "channel01",
-    uploader: "user01",
-    views: 15200,
-    uploadDate: "2024-09-20",
-  },
-  {
-    videoId: "video01",
-    title: "Learn React in 30 Minutes",
-    thumbnailUrl:
-      "https://images.unsplash.com/photo-1497015455546-1da71faf8d06?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    channelName: "Gaurang More",
-    channelId: "channel01",
-    uploader: "user01",
-    views: 15200,
-    uploadDate: "2024-09-20",
-  },
-  {
-    videoId: "video01",
-    title: "Learn React in 30 Minutes",
-    thumbnailUrl:
-      "https://images.unsplash.com/photo-1497015455546-1da71faf8d06?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    channelName: "Gaurang More",
-    channelId: "channel01",
-    uploader: "user01",
-    views: 15200,
-    uploadDate: "2024-09-20",
-  },
-  {
-    videoId: "video01",
-    title: "Learn React in 30 Minutes",
-    thumbnailUrl:
-      "https://images.unsplash.com/photo-1497015455546-1da71faf8d06?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    channelName: "Gaurang More",
-    channelId: "channel01",
-    uploader: "user01",
-    views: 15200,
-    uploadDate: "2024-09-20",
-  },
-];
+// const sampleData = [
+//   {
+//     videoId: "video01",
+//     title: "Learn React in 30 Minutes",
+//     thumbnailUrl:
+//       "https://images.unsplash.com/photo-1497015455546-1da71faf8d06?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//     channelName: "Gaurang More",
+//     channelId: "channel01",
+//     uploader: "user01",
+//     views: 15200,
+//     uploadDate: "2024-09-20",
+//   },
+//   {
+//     videoId: "video01",
+//     title: "Learn React in 30 Minutes",
+//     thumbnailUrl:
+//       "https://images.unsplash.com/photo-1497015455546-1da71faf8d06?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//     channelName: "Gaurang More",
+//     channelId: "channel01",
+//     uploader: "user01",
+//     views: 15200,
+//     uploadDate: "2024-09-20",
+//   },
+//   {
+//     videoId: "video01",
+//     title: "Learn React in 30 Minutes",
+//     thumbnailUrl:
+//       "https://images.unsplash.com/photo-1497015455546-1da71faf8d06?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//     channelName: "Gaurang More",
+//     channelId: "channel01",
+//     uploader: "user01",
+//     views: 15200,
+//     uploadDate: "2024-09-20",
+//   },
+//   {
+//     videoId: "video01",
+//     title: "Learn React in 30 Minutes",
+//     thumbnailUrl:
+//       "https://images.unsplash.com/photo-1497015455546-1da71faf8d06?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//     channelName: "Gaurang More",
+//     channelId: "channel01",
+//     uploader: "user01",
+//     views: 15200,
+//     uploadDate: "2024-09-20",
+//   },
+// ];
 
 const filter = [
   {
@@ -69,7 +72,25 @@ const filter = [
 ];
 const Home = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [activeTab, setActiveTab] = useState("all");
+
+  const [videos, setVideos] = useState([]);
+
+  const getVideos = () => {
+    dispatch(getAllVideosFunc())
+      .then(unwrapResult)
+      .then((res) => {
+        if (res.status) {
+          setVideos(res.data);
+        } else {
+          console.log("some error occured");
+        }
+      });
+  };
+  useEffect(() => {
+    getVideos();
+  }, []);
   return (
     <div className="ml-4">
       <div className="flex gap-4 mb-8">
@@ -92,7 +113,7 @@ const Home = () => {
         })}
       </div>
       <div className="text-white grid grid-cols-[repeat(auto-fit,_minmax(300px,_1fr))] gap-4 justify-center">
-        {sampleData.map((i) => {
+        {videos.map((i) => {
           return (
             <div
               key={i?.videoId}
